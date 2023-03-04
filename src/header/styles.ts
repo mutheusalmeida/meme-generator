ed2560f8183f402bb163c1958845a1450462a25b
Matheus <matheusalmeida016@gmail.com>
2023-03-04 18:27:57 -0400

    improve: header style

--- src/header/styles.ts
+++ src/header/styles.ts
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 export const HeaderWrapper = styled.header`
   padding: 1.6666em;
   background: var(--primary-gradient);
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  justify-content: center;
   align-items: center;
   font-weight: 500;
   font-size: 0.75rem;
   line-height: 1.25em;
+  grid-template-columns: max-content minmax(auto, 500px);
 
-  span:last-child {
+  > span:last-child {
     margin-right: 1.0625em;
+    justify-self: end;
   }
 `
 
